b99a3f20d086b5272a9a0c7cec3d90b5dc1b127f
Michael <93211335+waveyboym@users.noreply.github.com>
2022-11-22 15:06:58 +0200

    Update firebase.js

--- Source files/firebase.js
+++ Source files/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseApp = initializeApp ({
-    apiKey: "AIzaSyA5QPoMXkpbWf9cr-9IXPw-XuHuz_g3j5Q",
-    authDomain: "chat-app-project-4b80f.firebaseapp.com",
-    projectId: "chat-app-project-4b80f",
-    storageBucket: "chat-app-project-4b80f.appspot.com",
-    messagingSenderId: "9646833396",
-    appId: "1:9646833396:web:be42f16546e613b20968a1",
-    measurementId: "G-XZ4SCXF5JY"
+    apiKey: "<your API key from firebase>",
+    authDomain: "<your authDomain from firebase>",
+    projectId: "<your project ID from firebase>",
+    storageBucket: "<your storage bucket from firebase>",
+    messagingSenderId: "<your messaging sender ID from firebase>",
+    appId: "<your app ID from firebase>",
+    measurementId: "<your measurement ID from firebase>"
 });
 
 export const db = getFirestore();
