5c048972995af44638d4ed478f0c0a2d45ceaee1
Michael <93211335+waveyboym@users.noreply.github.com>
2023-03-05 14:01:07 +0200

    Add files via upload

---
+++ Fiddle Chat App src files(macos & linux)/src/projectAssets/index.js
@@ -0,0 +1,84 @@
+import facebooksvg from "./Facebook.svg";
+import app_banner_small from "./fiddle_chat_app_banner.png";
+import app_banner_small_LM from "./fiddle_chat_app_banner_LM.png";
+import app_logo_small from "./fiddle_chat_app_logo.svg";
+import app_logo_small_LM from "./fiddle_chat_app_logo_LM.svg";
+import githubsvg from "./Github.svg";
+import googlesvg from "./Google.svg";
+import social_friendsvg from "./social_friends.svg";
+import twittersvg from "./Twitter.svg";
+import blob from "./BlobVec.svg";
+import emailopened from "./email-opened.svg";
+import emailsent from "./email-sent.svg";
+
+//app-icons//
+import addfriend_LM from "./add-friend-LM.svg";
+import addfriend_DM from "./add-friend-DM.svg";
+import addfriend_SLCTD from "./add-friend-SLCTD.svg";
+import addfriend_green from "./add-friend-green.svg";
+
+import room_LM from "./rooms-LM.svg";
+import room_DM from "./rooms-DM.svg";
+import room_SLCTD from "./rooms-SLCTD.svg";
+
+import requests_LM from "./requests-LM.svg";
+import requests_DM from "./requests-DM.svg";
+import requests_SLCTD from "./requests-SLCTD.svg";
+
+import settings_LM from "./settings-LM.svg";
+import settings_DM from "./settings-DM.svg";
+import settings_SLCTD from "./settings-SLCTD.svg";
+
+import chatapplogo_IA from "./fiddle_chat_app_logo_in_app_small.svg";
+
+import deletefriend from "./deletefriend.svg";
+import user_LM from "./user-LM.svg";
+import user_DM from "./user-DM.svg";
+import dob_LM from "./DOB-LM.svg";
+import dob_DM from "./DOB-DM.svg";
+import pronouns_LM from "./pronouns-LM.svg";
+import pronouns_DM from "./pronouns-DM.svg";
+import sendmsg_LM from "./Send-LM.svg";
+import sendmsg_DM from "./Send-DM.svg";
+
+import themes_LM from "./Sun-LM.svg";
+import themes_DM from "./Sun-DM.svg";
+import about_LM from "./question-LM.svg";
+import about_DM from "./question-DM.svg";
+
+import upload from "./Cloud-upload.svg";
+import save from "./Check.svg";
+import logout from "./Logout.svg";
+import deleteprofile from "./Trash.svg";
+import google_small from "./Google-small.svg";
+import twitter_small from "./Twitter-small.svg";
+import facebook_small from "./Facebook-small.svg";
+import github_small from "./Github-small.svg";
+
+import eyeclosed_DM from "./Eye-closed-DM.svg";
+import eyeopened_DM from "./Eye-opened-DM.svg";
+import eyeclosed_LM from "./Eye-closed-LM.svg";
+import eyeopened_LM from "./Eye-opened-LM.svg";
+
+import join_room from "./join-room.svg";
+import create_room from "./create-room.svg";
+import leave_room from "./leave-room.svg";
+
+import openRoomArrow_DM from "./openRoomArrow-DM.svg";
+import openRoomArrow_LM from "./openRoomArrow-LM.svg";
+
+import emoji_DM from "./emoji-DM.svg";
+import emoji_LM from "./emoji-LM.svg"
+
+export { facebooksvg, app_banner_small, app_banner_small_LM, app_logo_small,
+    app_logo_small_LM, githubsvg, googlesvg, social_friendsvg, twittersvg,
+    blob, emailopened, emailsent, addfriend_LM, addfriend_DM, addfriend_SLCTD,
+    room_LM, room_DM, room_SLCTD, requests_LM, requests_DM, requests_SLCTD,
+    settings_LM, settings_DM, settings_SLCTD, chatapplogo_IA, deletefriend,
+    user_LM, user_DM, dob_LM, dob_DM, pronouns_LM, pronouns_DM, sendmsg_LM,
+    sendmsg_DM, themes_LM, themes_DM, about_LM, about_DM, upload, save,
+    logout, deleteprofile, google_small, twitter_small, facebook_small, github_small,
+    eyeclosed_DM, eyeopened_DM, eyeclosed_LM, eyeopened_LM, addfriend_green,
+    join_room, create_room, leave_room, openRoomArrow_LM, openRoomArrow_DM, emoji_DM, 
+    emoji_LM
+}
