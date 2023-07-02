137b3c8cf383e3b5195fc65d3ff9f8182b4bc236
Michael <93211335+waveyboym@users.noreply.github.com>
2023-07-02 18:55:30 +0200

    Add files via upload

---
+++ Fiddle Chat App/src-tauri/src/main.rs
@@ -0,0 +1,28 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+// Create the command:
+// This command must be async so that it doesn't run on the main thread.
+#[tauri::command]
+async fn close_splashscreen(window: tauri::Window) {
+  // Close splashscreen
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+  // Show main window
+  window.get_window("main").unwrap().show().unwrap();
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, close_splashscreen])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
